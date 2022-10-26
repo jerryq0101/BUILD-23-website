156e9b9521a224e63945ed235c864fb87250dd62
blobcode <lhelme123@gmail.com>
2022-10-25 23:27:14 -0400

    footer

--- src/partials/footer.js
+++ src/partials/footer.js
@@ -3,12 +3,30 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div>
-      <div className="flex items-center justify-center h-80 bg-neutral-800">
-        <div>
-          <p className="text-center text-3xl text-white">FOOTER</p>
+    <footer className="p-4 bg-neutral-800 sm:p-6 ">
+      <div className="mx-auto max-w-screen-xl">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <Link href="/" className="flex items-center">
+              <Image src="/logo2.svg" alt="Logo" width={48} height={48} />
+            </Link>
+            <p className="text-white py-5 max-w-xs">
+              If you have any questions or concerns, reach out at
+            </p>
+          </div>
+          <div className="flex items-center justify-center pb-2 text-center max-w-md md:text-right">
+            <p className="text-xl text-white">
+              We are working hard to bring you the best hack experience
+              possible, details being finalized - expect changes
+            </p>
+          </div>
+        </div>
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2022 Refoundation. All Rights Reserved.
+          </span>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
--- src/partials/header.js
+++ src/partials/header.js
@@ -3,23 +3,23 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <nav class="px-2 sm:px-4 pt-5">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <Link href="/" class="flex items-center">
+    <nav className="px-2 sm:px-4 pt-5">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link href="/" className="flex items-center">
           <Image src="/logo.svg" alt="Logo" width={48} height={48} />
-          <span class="self-center text-xl font-semibold whitespace-nowrap pl-3">
+          <span className="self-center text-xl font-semibold whitespace-nowrap pl-3">
             REFOUNDATION
           </span>
         </Link>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
           <svg
-            class="w-6 h-6"
+            className="w-6 h-6"
             aria-hidden="true"
             fill="currentColor"
             viewBox="0 0 20 20"
@@ -32,14 +32,22 @@ export default function Header() {
             ></path>
           </svg>
         </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
             <li>
               <Link
                 href="/"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0"
+                className="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0"
               >
-                About
+                PRE-REGISTRATION
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0"
+              >
+                CONTACT US
               </Link>
             </li>
           </ul>
