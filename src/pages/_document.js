41cd21a0af54a88efe6bb38bf2a2208b254007f7
jerryq0101 <jerryq0101@gmail.com>
2022-11-06 20:16:21 -0800

    Update for twitter sharing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,12 @@ function MyDocument() {
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.svg" />
+
+        {/* Twitter Sharing Look */}
+        <meta name="twitter:card" content="/Twitter-card.png" />
+        <meta name="twitter:title" content="BUILD'23 - Vancouver Hackathon" />
+        <meta name="twitter:description" content="BUILD, hosted by Refoundation, is a hackathon that engages the young minds from Vancouver to solve the most pressing issues of today's society." />
+        <meta name="twitter:image" content="/Twitter-card.png" />
       </Head>
       <body>
         <Main /> 
