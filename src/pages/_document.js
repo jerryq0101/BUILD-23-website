cbbc04362635cc49b785728e3d1bc2794db37328
jerryq0101 <jerryq0101@gmail.com>
2022-12-31 22:28:54 -0800

    meta cards

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,17 +13,17 @@ function MyDocument() {
 
         {/* Twitter Sharing Look */}
         <meta name="twitter:card" content="/Twitter-card.png" />
-        <meta name="twitter:title" content="BUILD'23 - Vancouver Hackathon" />
+        <meta name="twitter:title" content="BUILD'23 - Global Hackathon" />
         <meta
           name="twitter:description"
-          content="BUILD, hosted by Refoundation, is a hackathon that engages the young minds from Vancouver to solve the most pressing issues of today's society."
+          content="BUILD, hosted by Refoundation, is a hackathon that engages the young minds from the world to solve the most pressing issues of today's society."
         />
         <meta name="twitter:image" content="/Twitter-card.png" />
 
         <meta name="title" content="BUILD23" />
         <meta
           name="description"
-          content="BUILD, hosted by Refoundation, is a hackathon that engages the young minds from Vancouver to solve the most pressing issues of today's society."
+          content="BUILD, hosted by Refoundation, is a hackathon that engages the young minds from the world to solve the most pressing issues of today's society."
         />
         <meta
           name="keywords"
